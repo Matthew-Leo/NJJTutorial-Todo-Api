daa10afbaf9e8be260b26444180d7e99048d2d13
Matthew Leo <grumpynerd@yahoo.com>
2015-11-11 11:32:33 -0500

    Add todos and todos/id

--- server.js
+++ server.js
@@ -2,11 +2,42 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/' , function (req,res) {
+var todos = [{
+        id: 1,
+        description: "Meet E for lunch",
+        completed: false
+    }, {
+        id: 2,
+        description: "Go to market",
+        completed: false
+    }, {
+        id: 3,
+        description: "Master the secrets of time and space",
+        completed: true
+    }];
+
+app.get('/', function (req, res) {
     res.send('TODO API ROOT');
 });
 
+app.get('/todos', function (req, res) {
+    res.json(todos);
+})
 
-app.listen(PORT,function () {
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id);
+    var found = undefined;
+    todos.forEach(function (todo) {
+        if (todo.id === todoId) 
+            found = todo;
+    });
+    if (found)
+        res.json(found);
+    else
+        res.status(404).send();
+})
+
+
+app.listen(PORT, function () {
     console.log("express listening on port " + PORT);
 })
