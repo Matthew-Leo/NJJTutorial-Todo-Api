a15f5b22de8ce143128177c35c69e51153ddb9e0
Matthew Leo <grumpynerd@yahoo.com>
2015-11-16 15:01:46 -0500

    Add PUT /todos:id

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.get('/todos', function (req, res) {
     res.json(todos);
 });
 
+
+// GET /todos:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
     var found = _.findWhere(todos, {id: todoId});
@@ -27,14 +29,55 @@ app.get('/todos/:id', function (req, res) {
         res.status(404).send();
 });
 
+// DELETE /todos:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
     var found = _.findWhere(todos, {id: todoId});
     if (found) {
-        todos = _.without(todos,found)
+        todos = _.without(todos, found)
         res.json(found);
     } else
-        res.status(404).json({error:"No todo found with id:" + req.params.id});
+        res.status(404).json({error: "No todo found with id:" + req.params.id});
+});
+
+// PUT /todos:id
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id);
+    var found = _.findWhere(todos, {id: todoId});
+    if (!found) {
+        console.log("Did not find id " + todoId);
+        res.status(404).send(); 
+        return;
+    }
+    var body = req.body;
+    var validAttributes = {};
+    console.log("BODY:" + JSON.stringify(req.body));
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        console.log("Setting completed:" + body.completed);
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        // bad property
+        console.log("something screwy with completed property");
+        res.status(400).send();
+        return;
+    } 
+    if (body.hasOwnProperty('description') 
+            && _.isString(body.description) 
+            && body.description.trim().length > 0) {
+        console.log("Setting description:" + body.description);
+        
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        // bad property
+        console.log("something screwy with description property");
+        res.status(400).send();
+        return;
+    } 
+   console.log(JSON.stringify(validAttributes));
+   _.extend(found, validAttributes);
+   console.log(JSON.stringify(found));
+   res.status(200).json(found) 
+    
 });
 
 // POST /todos
@@ -58,9 +101,9 @@ app.post("/todos", function (req, res) {
 
 try {
     console.log("about to listen on port " + PORT);
-app.listen(PORT, function () {
-    console.log("express listening on port " + PORT);
-});
+    app.listen(PORT, function () {
+        console.log("express listening on port " + PORT);
+    });
 } catch (e) {
     console.log("CAUGHT:" + e.toString());
 } finally {
