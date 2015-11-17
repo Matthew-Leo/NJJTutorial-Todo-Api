c012d6b473542a8fe8c4d9fbf98770fdf42120d2
Matthew Leo <grumpynerd@yahoo.com>
2015-11-17 16:07:54 -0500

    Update CRUD REST routes in server.js to use Sequelize.

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ var _ = require("underscore");
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var db = require('./db.js');
 
 
-var todos = []; // start with empty todos list
-var todoNextId = 1;
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -16,51 +15,60 @@ app.get('/', function (req, res) {
 
 // get /todos?completed=true&q=descriptionContains
 app.get('/todos', function (req, res) {
+    console.log("GET /todos");
     var queryParams = req.query;
-    var filteredTodos = todos;
+    var filter = {where: {}};
     if (queryParams.hasOwnProperty('completed')) {
         var completed;
-        if (queryParams.completed === "true") 
+        if (queryParams.completed === "true")
             completed = true;
         else if (queryParams.completed === "false")
             completed = false;
         else {
-            res.status(400).json({error : "invalid value for completed:" + queryParams.completed});
+            res.status(400).json({error: "invalid value for completed:" + queryParams.completed});
             return;
         }
-        filteredTodos = _.where(filteredTodos,{completed : completed});
+        filter.where.completed = completed;
     }
-    if (queryParams.hasOwnProperty('q') && 
+    if (queryParams.hasOwnProperty('q') &&
             queryParams.q.trim().length > 0) {
         var lookingFor = queryParams.q.trim().toLowerCase();
-        filteredTodos = _.filter(filteredTodos, function (item) {
-            return (item.hasOwnProperty('description') &&
-                    item.description.toLowerCase().indexOf(lookingFor) >= 0)
-        })
+        filter.where.description = {$like: "%" + lookingFor + "%"};
     }
-    res.json(filteredTodos);
+    db.todo.findAll(filter).then(function (found) {
+        res.json(found);
+    }).catch(function (e) {
+        res.status(400).json(e);
+    })
 });
 
 
 // GET /todos:id
 app.get('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
-    var found = _.findWhere(todos, {id: todoId});
-    if (found)
-        res.json(found);
-    else
-        res.status(404).send();
+    console.log("looking for " + todoId);
+    db.todo.find({where: {id: todoId}}).then(function (found) {
+        console.log(JSON.stringify(found));
+        res.json(found)
+    }).catch(function (err) {
+        console.log(err);
+        res.status(400).json(err);
+    })
 });
 
 // DELETE /todos:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
-    var found = _.findWhere(todos, {id: todoId});
-    if (found) {
-        todos = _.without(todos, found)
-        res.json(found);
-    } else
-        res.status(404).json({error: "No todo found with id:" + req.params.id});
+    db.todo.find({where: {id: todoId}}).
+            then(function (found) {
+                return found.destroy();
+            }).
+            then(function (found) {
+                res.status(200).json(found);
+            }).
+            catch(function (err) {
+                res.status(404).json(err);
+            });
 });
 
 
@@ -75,20 +83,21 @@ app.post("/todos", function (req, res) {
     }
     var cleanPostedObj = _.pick(body, "description", "completed");
     cleanPostedObj.description = cleanPostedObj.description.trim();
-    cleanPostedObj["id"] = todoNextId++;
-    todos.push(cleanPostedObj);
-    console.log(cleanPostedObj.description);
-    res.send(cleanPostedObj);
+    db.todo.create(cleanPostedObj).then(
+            function (newObj) {
+                console.log(JSON.stringify(newObj));
+                res.status(200).json(newObj);
+            }).catch(
+            function (err) {
+                console.log("Error:");
+                console.log(err);
+                res.status(400).json(err);
+            });
 });
 
 // note -- "PUT" in the course API should be "PATCH".
 function patchFunc(req, res) {
     var todoId = parseInt(req.params.id);
-    var found = _.findWhere(todos, {id: todoId});
-    if (!found) {
-        res.status(404).send();
-        return;
-    }
 
     var body = req.body;
     var validAttributes = {};
@@ -105,12 +114,20 @@ function patchFunc(req, res) {
         validAttributes.description = body.description;
     } else if (body.hasOwnProperty('description')) {
         console.log("bad description")
-        res.status(400).json({error: "invalid value type for completed:'" + body.description + "'"});
+        res.status(400).json({error: "invalid value type for description:'" + body.description + "'"});
         return;
     }
-    _.extend(found, validAttributes);
-    res.status(200).json(found);
-
+    // OK now try the update
+    db.todo.findById(todoId).
+            then(function (found) {
+                _.extend(found, validAttributes); //could also use update
+                found.save().then(function () {
+                    res.status(200).json(found); //wait for db success
+                });
+            }).catch(function (err) {
+        console.log(err);
+        res.status(400).json(err);
+    });
 }
 ;
 
@@ -120,12 +137,17 @@ app.patch('/todos/:id', patchFunc);
 // PUT /todos:id
 app.put('/todos/:id', patchFunc);
 
+/*
+ * set up database
+ */
+db.sequelize.sync().then(function () {
+    try {
+        console.log("about to listen on port " + PORT);
+        app.listen(PORT, function () {
+            console.log("express listening on port " + PORT);
+        });
+    } catch (e) {
+        console.log("CAUGHT:" + e.toString());
+    }
+});
 
-try {
-    console.log("about to listen on port " + PORT);
-    app.listen(PORT, function () {
-        console.log("express listening on port " + PORT);
-    });
-} catch (e) {
-    console.log("CAUGHT:" + e.toString());
-}
